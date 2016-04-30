f50db014dac4d83423cb614b8ccde77732706794
John Brawner <JWB@Johns-MacBook-Pro.local>
2016-04-29 21:08:49 -0700

    Adding basic main menu

---
+++ Project/Assets/Scripts/MainMenu/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour {
+
+	public void ButtonMenu(Button button)
+	{
+		if (button.name == "Play") {
+			print ("play");
+			Application.LoadLevel("level1");
+		}
+		if (button.name == "Upgrades") {
+			print ("Upgrades");
+		}
+		if (button.name == "Exit") {
+			print ("Exit");
+		}
+
+
+	}
+}
