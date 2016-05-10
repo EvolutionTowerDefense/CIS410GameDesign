510414893da4331a697297d31ba56384c4dc256d
rsapping <rsapping@uoregon.edu>
2016-05-10 16:00:16 -0700

    Update game - menu missing parts

---
+++ Project/Project/Assets/Scripts/MainMenu/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	public void ButtonMenu(Button button)
+	{
+		if (button.name == "Play") {
+			print ("play");
+			SceneManager.LoadScene("level1");
+		
+
+		}
+		if (button.name == "Upgrades") {
+			print ("Upgrades");
+		}
+		if (button.name == "Exit") {
+			print ("Exit");
+		}
+
+
+	}
+}
