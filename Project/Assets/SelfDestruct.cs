2e5950a8ac8a01fb25609e5bbab7f1f9daf99547
John Brawner <JWB@128-223-223-127-wireless.uoregon.edu>
2016-05-17 20:29:25 -0700

    fixed range possibly

--- Project/Assets/SelfDestruct.cs
+++ Project/Assets/SelfDestruct.cs
@@ -10,7 +10,7 @@ public class SelfDestruct : MonoBehaviour {
 	void Start () {
 		float temp = size.GetComponent <gunTower>().fireRadius;
 		//Scales
-		temp = temp * 2.45f;
+		temp = temp * 4.275f;//was 2.45f
 		transform.localScale += new Vector3(temp,temp,temp);
 		Object.Destroy(gameObject, 2.5f);
 	}
