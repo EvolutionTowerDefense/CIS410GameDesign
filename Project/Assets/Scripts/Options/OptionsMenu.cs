391ead6078ab3b3260c5f2718045e2b97c927919
John Brawner <JWB@128-223-223-127-wireless.uoregon.edu>
2016-05-19 18:44:04 -0700

    Made sliders in options show real slider position of volume if person goes in it multiple times, does not persist through leaving game

--- Project/Assets/Scripts/Options/OptionsMenu.cs
+++ Project/Assets/Scripts/Options/OptionsMenu.cs
@@ -2,23 +2,29 @@
 using System.Collections;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.Audio;
 
 public class OptionsMenu : MonoBehaviour {
-	public GameObject options;
-	private int musicVol;
-	private int sfxVol;
-	void Start () {
-		//musicVol = 
-		//sfxVol = 
+	public Slider musicSilder;
+	public Slider sfxSlider;
+	public AudioMixer masterMixer;
+	float musicVol;
+	float sfxVol;
+	void Start () {		
+		masterMixer.GetFloat("musicVol", out( musicVol));
+		masterMixer.GetFloat ("sfxVol", out(sfxVol));
+		musicSilder.value = musicVol;
+		sfxSlider.value = sfxVol;
 	}
+
 	public void ButtonMenu(Button button)
 	{
+		/*
 		if (button.name == "Apply") {
 			print ("Apply");
-
-
 			SceneManager.LoadScene("MainMenu");
 		}
+		*/
 		if (button.name == "MainMenu") {
 			print ("Main");
 			SceneManager.LoadScene ("MainMenu");
