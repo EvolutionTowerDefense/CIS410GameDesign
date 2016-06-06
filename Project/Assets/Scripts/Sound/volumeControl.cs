3c2a20ce379dac17e39a4fd2e443d9728277481c
rsapping <rsapping@uoregon.edu>
2016-06-05 23:18:40 -0700

    Fixed volumolume bug, can now scale all levels properly

--- Project/Assets/Scripts/Sound/volumeControl.cs
+++ Project/Assets/Scripts/Sound/volumeControl.cs
@@ -6,16 +6,36 @@ using UnityEngine.Audio;
 public class volumeControl : MonoBehaviour {
 
 	public AudioMixer masterMixer;
+	public Slider MusicSlider;
+	public Slider SFX;
+
+	void Start(){
+
+		SFX.value= PlayerPrefs.GetFloat("sfxVol");
+		MusicSlider.value=PlayerPrefs.GetFloat("musicVol");
+		SetSfxLvl (PlayerPrefs.GetFloat ("sfxVol"));
+		SetMusicLvl (PlayerPrefs.GetFloat("musicVol"));
+
+	}
+
+
+
+
 
 	public void SetSfxLvl(float sfxLvl)
 	{
 		masterMixer.SetFloat ("sfxVol",sfxLvl);
+		PlayerPrefs.SetFloat("sfxVol", sfxLvl);
 	}
+
 	public void SetMusicLvl (float musicLvl)
 	{
 		masterMixer.SetFloat ("musicVol", musicLvl);
+		PlayerPrefs.SetFloat("musicVol", musicLvl);
 	}
 
 
 
 }
+
+
